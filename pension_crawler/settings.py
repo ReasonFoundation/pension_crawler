21904212c0c6aaa8fba8fd3a832cdb4d6de23925
tishmen <topuzov.work@gmail.com>
2018-01-30 15:58:50 +0100

    changed downloads dir

--- pension_crawler/settings.py
+++ pension_crawler/settings.py
@@ -11,14 +11,14 @@ ITEM_PIPELINES = {
     'scrapy.pipelines.files.FilesPipeline': 1,
     'pension_crawler.pipelines.ResultItemCSVExportPipeline': 900
 }
-FILES_STORE = os.path.join(os.getcwd(), 'downloads')
+FILES_STORE = os.path.join(os.getcwd(), 'data', 'downloads')
+FIELDS_TO_EXPORT = ['keyword', 'url', 'title', 'path']
 
 
 # custom settings
 
-INPUT_FILE = 'input.csv'
-OUTPUT_FILE = 'output.csv'
-FIELDS_TO_EXPORT = ['keyword', 'url', 'title', 'path']
+INPUT_FILE = os.path.join(os.getcwd(), 'data', 'input.csv')
+OUTPUT_FILE = os.path.join(os.getcwd(), 'data', 'output.csv')
 
 SEARCH_ENGINE_ID = os.getenv('SEARCH_ENGINE_ID')
 API_KEY = os.getenv('API_KEY')
