dcf443969b87b38008b73191d197e7db570e1815
tishmen <topuzov.work@gmail.com>
2018-02-09 14:10:57 +0100

    more custom settings

--- pension_crawler/google/settings.py
+++ pension_crawler/google/settings.py
@@ -2,7 +2,8 @@
 
 import os
 
-from pension_crawler.settings import DATA_DIR
+from pension_crawler.settings import DATA_DIR, NO_DOWNLOAD
+from pension_crawler.utils import no_download
 
 
 SETTINGS = {
@@ -15,5 +16,10 @@ SETTINGS = {
 
     'OUTPUT_FILE': os.path.join(DATA_DIR, 'google', 'output.csv'),
     'SEARCH_ENGINE_ID': os.getenv('GOOGLE_SEARCH_ENGINE_ID'),
-    'API_KEY': os.getenv('GOOGLE_API_KEY')
+    'API_KEY': os.getenv('GOOGLE_API_KEY'),
+    'RESULT_DEPTH': 0,
+    'KEYWORD_MODIFIER': 'actuarial valuation'
 }
+
+if NO_DOWNLOAD:
+    SETTINGS = no_download(SETTINGS)
