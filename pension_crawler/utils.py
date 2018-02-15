859eeb3447bc69221b60fa80ccb41655501f035b
tishmen <topuzov.work@gmail.com>
2018-02-15 16:15:31 +0100

    redeveloped utils

--- pension_crawler/utils.py
+++ pension_crawler/utils.py
@@ -1,14 +1,28 @@
 '''utils.py'''
 
+import json
+import logging
+
 from scrapy.exceptions import NotConfigured
 
 
-def no_download(settings):
-    '''Disable fields used for downloading and exporting files to csv.'''
-    settings['ITEM_PIPELINES'] = {
-        'pension_crawler.pipelines.PDFNoDownloadPipeline': 900
+logger = logging.getLogger(__name__)
+
+
+def get_download_settings(download):
+    '''Return search settings depending on whether download is enabled.'''
+    item_pipelines = {
+        'scrapy.pipelines.files.FilesPipeline': 1,
+        'pension_crawler.pipelines.SearchExportDownloadEnabledPipeline': 300
     }
-    return settings
+    fields_to_export = ['keyword', 'url', 'title', 'path']
+    if not download:
+        item_pipelines = {
+            'pension_crawler.pipelines.SearchExportDownloadDisabledPipeline':
+                300
+        }
+        fields_to_export = ['keyword', 'url', 'title']
+    return item_pipelines, fields_to_export
 
 
 class SpiderMixin(object):
@@ -16,25 +30,44 @@ class SpiderMixin(object):
     '''Common spider functionality.'''
 
     @staticmethod
-    def parse_keywords(path):
-        '''Read search keywords from input file.'''
-        keywords = []
-        try:
-            with open(path, 'r') as file_:
-                for line in file_.readlines():
-                    keywords.append(line.strip().lower())
-        except IOError:
-            raise NotConfigured('Could not read input file.')
+    def get_list_from_file(settings):
+        '''Return values from file input.'''
+        input_file = settings.get('INPUT_FILE')
+        with open(input_file, 'r') as file_:
+            return [i.strip() for i in file_.readlines()]
+
+    @staticmethod
+    def get_list_from_args(args):
+        '''Return values from spider arguments.'''
+        keywords = args.pop('keywords')
         if not keywords:
-            raise NotConfigured('Keyword list is empty.')
+            return
+        try:
+            return json.loads(keywords)
+        except ValueError:
+            raise NotConfigured('Keywords list from args is invalid.')
+
+    @staticmethod
+    def get_keywords(args, settings):
+        keywords = SpiderMixin.get_list_from_args(args)
+        if not keywords:
+            logger.info('Keyword list from args is empty.')
+            keywords = SpiderMixin.get_list_from_file(settings)
+        if not keywords:
+            raise NotConfigured('Keyword list from file is empty.')
         return keywords
 
     @staticmethod
-    def parse_settings(settings):
+    def parse_common_settings(args, settings):
         '''Parse common settings for all spiders.'''
-        input_file = settings.get('INPUT_FILE')
+        keywords = SpiderMixin.get_keywords(args, settings)
         depth = settings.get('RESULT_DEPTH')
         modifier = settings.get('KEYWORD_MODIFIER')
-        if not input_file:
-            raise NotConfigured('Input file not set.')
-        return input_file, depth, modifier
+        return keywords, depth, modifier
+
+    def get_query(self, query):
+        '''Return search query.'''
+        modifier = 'filetype:pdf'
+        if self.modifier:
+            modifier = '{} {}'.format(self.modifier, modifier)
+        return '{} {}'.format(query, modifier)
