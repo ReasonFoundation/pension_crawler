a4beb4673dbf8581fa0e6713c4dee32b3c5ca338
tishmen <topuzov.work@gmail.com>
2018-02-15 16:12:57 +0100

    redeveloped items and loaders

--- pension_crawler/items.py
+++ pension_crawler/items.py
@@ -2,7 +2,7 @@
 
 from scrapy import Item, Field
 from scrapy.loader import ItemLoader
-from scrapy.loader.processors import TakeFirst
+from scrapy.loader.processors import TakeFirst, Identity
 
 
 class ResultItem(Item):
@@ -19,9 +19,35 @@ class ResultItem(Item):
     timestamp = Field()
 
 
-class ResultItemLoader(ItemLoader):
+class PDFItem(Item):
+
+    '''Pdf item.'''
+
+    url = Field()
+    href = Field()
+    text = Field()
+    file_urls = Field()
+    files = Field()
+    timestamp = Field()
+
+
+class BaseLoader(ItemLoader):
+
+    '''Base item loader.'''
+
+    default_output_processor = TakeFirst()
+    file_urls_out = Identity()
+
+
+class ResultLoader(BaseLoader):
 
     '''Result item loader.'''
 
     default_item_class = ResultItem
-    default_output_processor = TakeFirst()
+
+
+class PDFLoader(BaseLoader):
+
+    '''PDF item loader.'''
+
+    default_item_class = PDFItem
