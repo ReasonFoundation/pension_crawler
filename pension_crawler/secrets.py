2fb42c793b850cae19be00c9e0bcf57d9bfba1bc
tishmen <topuzov.work@gmail.com>
2018-02-19 11:25:41 +0100

    added secrets to repo

---
+++ pension_crawler/secrets.py
@@ -0,0 +1,16 @@
+'''secrets.py'''
+
+
+# Change in production!
+
+SECRETS = {
+
+    # google spider
+
+    'GOOGLE_SEARCH_ENGINE_ID': '006550828608930697575:rdedpbbuy94',
+    'GOOGLE_API_KEY': 'AIzaSyDQmBGbFQBXdBCz94lNWHV86KEdrAFdQfw',
+
+    # bing spider
+
+    'BING_API_KEY': 'ddb0cdbe8bbe408db2035d4328b7eec9'
+}
