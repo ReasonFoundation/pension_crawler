e1b6c35080dc5efd6912b259c1613aecafee506e
tishmen <topuzov.work@gmail.com>
2018-02-22 13:48:59 +0100

    reworked google settings

--- pension_crawler/google/settings.py
+++ pension_crawler/google/settings.py
@@ -2,29 +2,29 @@
 
 import os
 
-from pension_crawler.settings import DATA_DIR
-
 from pension_crawler.secrets import SECRETS
-from pension_crawler.utils import get_download_settings
+from pension_crawler.settings import DATA_DIR
+from pension_crawler.utils import Settings
 
 
 # set download to False to disable PDF downloading from search results
 
 DOWNLOAD = True
-
-item_pipelines, fields_to_export = get_download_settings(DOWNLOAD)
+settings = Settings(DOWNLOAD)
 
 SETTINGS = {
 
     # Custom settings
 
-    'INPUT_FILE': os.path.join(DATA_DIR, 'input', 'google', 'default.txt'),
+    'keyword_file': 'keywords.txt',
+    'site_file': 'sites.txt',
+    'modifier': 'actuarial valuation',
+    'start_date': None,  # Required format: YYYYMMDD
+    'end_date': None,  # Required format: YYYYMMDD
+    'depth': 0,
+
+    'INPUT_DIR': os.path.join(DATA_DIR, 'input', 'google'),
     'OUTPUT_DIR': os.path.join(DATA_DIR, 'output', 'google'),
-    'KEYWORD_MODIFIER': 'actuarial valuation',
-    'SITE': None,
-    'START_DATE': None,  # Required format: YYYYMMDD
-    'END_DATE': None,  # Required format: YYYYMMDD
-    'RESULT_DEPTH': 0,
 
     # Secret settings
 
@@ -33,8 +33,8 @@ SETTINGS = {
 
     # Scrapy settings
 
-    'ITEM_PIPELINES': item_pipelines,
-    'FIELDS_TO_EXPORT': fields_to_export,
+    'ITEM_PIPELINES': settings.item_pipelines,
+    'FIELDS_TO_EXPORT': settings.fields_to_export,
     'COOKIES_ENABLED': False,
     'RETRY_ENABLED': False,
     'DOWNLOAD_TIMEOUT': 90,
