ffc97e7a6f8af60e021515c85292a556c312b4d1
tishmen <topuzov.work@gmail.com>
2018-02-22 18:04:20 +0100

    fixes for base and query parser

--- pension_crawler/utils.py
+++ pension_crawler/utils.py
@@ -19,25 +19,29 @@ class BaseParser(object):
     # constructor
 
     def __init__(self, args, settings):
-        '''Set args and settings.'''
+        '''Set args, settings and input directory.'''
         self.args = args
         self.settings = settings
+        input_dir = self._from_settings('input_dir')
+        if not input_dir:
+            raise NotConfigured('No input directory specified.')
+        self.input_dir = input_dir
 
     # private methods
 
     def _from_args(self, key):
-        '''Retrieve value from spider arguments.'''
-        value = self.args.pop(key)
-        if not value:
-            logger.debug('Value {} not found in arguments.'.format(key))
-        return value
+        '''Safe retrieve value from spider arguments.'''
+        try:
+            return self.args.pop(key)
+        except KeyError:
+            print('Key {} not found in spider arguments.'.format(key))
 
     def _from_settings(self, key):
-        '''Retrieve value from spider settings.'''
-        value = self.settings.get(key)
-        if not value:
-            logger.debug('Value {} not found in settings.'.format(key))
-        return value
+        '''Safe retrieve value from spider settings.'''
+        try:
+            return self.settings[key]
+        except KeyError:
+            print('Key {} not found in spider settings.'.format(key))
 
     def _from_args_or_settings(self, key):
         '''Retrieve value from spider arguments or settings.'''
@@ -48,6 +52,7 @@ class BaseParser(object):
         try:
             return json.loads(text)
         except ValueError:
+            print('JSON conversion error.')
             return []
 
     def _from_file(self, path):
@@ -63,30 +68,30 @@ class SearchParser(BaseParser):
     # constructor
 
     def __init__(self, *args, **kwargs):
-        '''Set input directory and filetype.'''
+        '''Set modifier and filetype.'''
         super(SearchParser, self).__init__(*args, **kwargs)
-        input_dir = self._from_settings('input_dir')
-        modifier = self._from_settings('modifier')
-        if not input_dir:
-            raise NotConfigured('Input directory not found in settings.')
+        modifier = self._from_args_or_settings('modifier')
+        filetype = self._from_args_or_settings('filetype')
         if not modifier:
-            raise NotConfigured('Query modifier not found in settings.')
-        self.input_dir = input_dir
+            raise NotConfigured('No query modifier specified.')
+        if not filetype:
+            raise NotConfigured('No query filetype specified.')
         self.modifier = modifier
-        self.filetype = 'filetype:pdf'
+        self.filetype = filetype
 
     # private methods
 
     def _query(self, keyword, modifier):
         '''Return joined query.'''
-        return ' '.join([keyword, modifier, self.filetype])
+        query = [keyword, modifier, 'filetype:{}'.format(self.filetype)]
+        return ' '.join(query)
 
-    def _keyword_queries(self, lst):
-        '''Return keyword queries.'''
+    def _keyword_query_list(self, lst):
+        '''Return keyword query list.'''
         return [self._query(i, self.modifier) for i in lst]
 
-    def _site_queries(self, lst):
-        '''Return site queries.'''
+    def _site_query_list(self, lst):
+        '''Return site query list.'''
         return [self._query(self.modifier, i) for i in lst]
 
     # properties
@@ -104,16 +109,16 @@ class SearchParser(BaseParser):
         '''Return input list.'''
         keyword_list = self._from_args('keyword_list')
         if keyword_list:
-            return self._keyword_queries(self._from_json(keyword_list))
+            return self._keyword_query_list(self._from_json(keyword_list))
         site_list = self._from_args('site_list')
         if site_list:
-            return self._site_queries(self._from_json(site_list))
+            return self._site_query_list(self._from_json(site_list))
         keyword_file = self._from_args_or_settings('keyword_file')
         if keyword_file:
-            return self._keyword_queries(self._from_file(keyword_file))
+            return self._keyword_query_list(self._from_file(keyword_file))
         site_file = self._from_args_or_settings('site_file')
         if site_file:
-            return self._site_queries(self._from_file(site_file))
+            return self._site_query_list(self._from_file(site_file))
         raise NotConfigured('Input list not configured.')
 
     @property
