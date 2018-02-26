d2e1f52b37c664d1c35debcd481aa8fa4d023906
tishmen <topuzov.work@gmail.com>
2018-02-26 12:33:53 +0100

    added user agent middleware and refactoring

--- pension_crawler/middlewares.py
+++ pension_crawler/middlewares.py
@@ -1,5 +1,7 @@
 '''middlewares.py'''
 
+import random
+
 import tldextract
 
 from scrapy.exceptions import NotConfigured, IgnoreRequest
@@ -44,3 +46,38 @@ class BlacklistMiddleware(object):
         parsed = tldextract.extract(request.url)
         if '.'.join((parsed.domain, parsed.suffix)) in self.data:
             raise IgnoreRequest()
+
+
+class UserAgentMiddleware(object):
+
+    '''Random user agent on every request.'''
+
+    # constructor
+
+    def __init__(self, user_agents, *args, **kwargs):
+        '''Set user agent list.'''
+        super(UserAgentMiddleware, self).__init__(*args, **kwargs)
+        self.user_agents = user_agents
+
+    # class methods
+
+    @classmethod
+    def from_crawler(cls, crawler, *args, **kwargs):
+        '''Pass data to constructor.'''
+        user_agents = crawler.settings.get('USER_AGENTS')
+        if not user_agents:
+            raise NotConfigured('User agent list is empty.')
+        return cls(user_agents, *args, **kwargs)
+
+    # properties
+
+    @property
+    def user_agent(self):
+        '''Get random user agent from list.'''
+        return random.choice(self.user_agents)
+
+    # overriden class methods
+
+    def process_request(self, request, *args, **kwargs):
+        '''Set random user agent in request headers.'''
+        request.headers.setdefault('User-Agent', self.user_agent)
