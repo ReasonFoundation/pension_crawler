05b4185e1045073d587b03437aaa1472a9f5ef74
tishmen <topuzov.work@gmail.com>
2018-02-26 13:18:47 +0100

    refactored csv pipeline

--- pension_crawler/pipelines.py
+++ pension_crawler/pipelines.py
@@ -8,37 +8,52 @@ from scrapy.exceptions import NotConfigured
 from scrapy.exporters import CsvItemExporter
 
 
-class BaseCSVPipeline(object):
+class CSVPipeline(object):
 
-    '''Common CSV pipeline functionality.'''
+    '''Export items to CSV.'''
 
-    def __init__(self, output_file, fields_to_export, *args, **kwargs):
+    # constructor
+
+    def __init__(self, path, fields, *args, **kwargs):
         '''Set output file object, fields to export and CSV exporter.'''
-        self.file = open(output_file, 'w+b')
-        self.exporter = CsvItemExporter(
-            self.file, fields_to_export=fields_to_export
-        )
+        self.file_ = open(path, 'w+b')
+        self.exporter = CsvItemExporter(self.file_, fields_to_export=fields)
 
-    @staticmethod
-    def get_settings(settings):
-        '''Read spider settings.'''
-        output_dir = settings.get('OUTPUT_DIR')
-        fields_to_export = settings.get('FIELDS_TO_EXPORT')
-        if not output_dir:
-            raise NotConfigured('Output directory not set.')
-        if not fields_to_export:
-            raise NotConfigured('Fields to export not set.')
-        timestamp = datetime.now().strftime('%Y-%m-%d-%H-%M')
-        output_file = os.path.join(output_dir, '{}.csv'.format(timestamp))
-        return output_file, fields_to_export
+    # class methods
 
     @classmethod
     def from_crawler(cls, crawler, *args, **kwargs):
         '''Pass output file and fields to export to constructor.'''
-        output_file, fields_to_export = BaseCSVPipeline.get_settings(
-            crawler.settings
-        )
-        return cls(output_file, fields_to_export, *args, **kwargs)
+        output_dir = crawler.settings.get('OUTPUT_DIR')
+        fields_to_export = crawler.settings.get('FIELDS_TO_EXPORT')
+        if not output_dir:
+            raise NotConfigured('Output directory not specified.')
+        if not fields_to_export:
+            raise NotConfigured('Fields to export not specified.')
+        timestamp = datetime.now().strftime('%Y-%m-%d-%H-%M')
+        path = os.path.join(output_dir, '{}.csv'.format(timestamp))
+        return cls(path, fields_to_export, *args, **kwargs)
+
+    # private methods
+
+    def _path(self, item):
+        '''Return path or none.'''
+        try:
+            return item['files'][0]['path']
+        except IndexError:
+            return ''
+
+    def _export(self, item):
+        '''Export row to CSV file.'''
+        data = {}
+        for key in self.exporter.fields_to_export:
+            if key == 'path':
+                data[key] = self._path(item)
+            else:
+                data[key] = item[key]
+        self.exporter.export_item(data)
+
+    # overriden class methods
 
     def open_spider(self, *args, **kwargs):
         '''Start exporting items on signal.'''
@@ -47,64 +62,9 @@ class BaseCSVPipeline(object):
     def close_spider(self, *args, **kwargs):
         '''Stop exporting items on signal.'''
         self.exporter.finish_exporting()
-        self.file.close()
+        self.file_.close()
 
     def process_item(self, item, *args, **kwargs):
         '''Export item to csv file and return item.'''
-        self.export_item(item)
+        self._export(item)
         return item
-
-    def get_path(self, item):
-        '''Return path or none.'''
-        try:
-            return item['files'][0]['path']
-        except IndexError:
-            pass
-
-    def export_item(self, item):
-        '''Export row to CSV file.'''
-        raise NotImplementedError
-
-
-class SearchExportDownloadEnabledPipeline(BaseCSVPipeline):
-
-    '''Pipeline that exports items to CSV file when PDF download is enabled.'''
-
-    def export_item(self, item, *args, **kwargs):
-        '''Export item to csv file.'''
-        data = {
-            'keyword': item['keyword'],
-            'url': item['url'],
-            'title': item['title'],
-            'path': self.get_path(item)
-        }
-        self.exporter.export_item(data)
-
-
-class SearchExportDownloadDisabledPipeline(BaseCSVPipeline):
-
-    '''Pipeline that exports items to CSV file when PDF download is disabled.'''
-
-    def export_item(self, item, *args, **kwargs):
-        '''Export item to csv file.'''
-        data = {
-            'keyword': item['keyword'],
-            'url': item['url'],
-            'title': item['title']
-        }
-        self.exporter.export_item(data)
-
-
-class SitesExportPipeline(BaseCSVPipeline):
-
-    '''Pipeline that exports items to CSV file from sites crawler.'''
-
-    def export_item(self, item, *args, **kwargs):
-        '''Export item to csv file.'''
-        data = {
-            'url': item['url'],
-            'href': item['href'],
-            'text': item.get('text'),
-            'path': self.get_path(item)
-        }
-        self.exporter.export_item(data)
