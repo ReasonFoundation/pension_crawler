f55fa8ebe71f7bea4561db7ef675b7bcd7b6ff43
tishmen <topuzov.work@gmail.com>
2018-02-26 12:16:35 +0100

    refactored sites spider

--- pension_crawler/sites/spiders.py
+++ pension_crawler/sites/spiders.py
@@ -1,23 +1,17 @@
 '''spiders.py'''
 
-import json
-import logging
-
 from datetime import datetime
 from urllib.parse import urlparse, urlunparse
 
-from scrapy import Spider, Request
+from scrapy import Request
 
 from pension_crawler.items import PDFLoader
-from pension_crawler.utils import SitesParser
+from pension_crawler.utils import BaseSpider
 
 from .settings import SETTINGS
 
 
-logger = logging.getLogger(__name__)
-
-
-class SitesSpider(Spider):
+class SitesSpider(BaseSpider):
 
     '''Extract pdf files from a list of sites.'''
 
@@ -28,23 +22,23 @@ class SitesSpider(Spider):
 
     # constructor
 
-    def __init__(self, crawler, input_list, *args, **kwargs):
+    def __init__(self, crawler, data, *args, **kwargs):
         '''Set crawler and input list.'''
         super(SitesSpider, self).__init__(*args, **kwargs)
         self.crawler = crawler
-        self.input_list = input_list
+        self.data = data
 
     # class methods
 
     @classmethod
     def from_crawler(cls, crawler, *args, **kwargs):
         '''Pass crawler and start urls to constructor.'''
-        parser = SitesParser(kwargs, crawler.settings)
-        return cls(crawler, parser.input_list,  *args, **kwargs)
+        data = SitesSpider._data(crawler.settings)
+        return cls(crawler, data, *args, **kwargs)
 
     # private methods
 
-    def _get_href(self, url, node):
+    def _href(self, url, node):
         '''Get href full location.'''
         parsed_url = urlparse(url)
         href = node.xpath('@href').extract_first()
@@ -54,9 +48,9 @@ class SitesSpider(Spider):
             parsed_href = parsed_href._replace(netloc=parsed_url.netloc)
         return urlunparse(parsed_href)
 
-    def _load_item(self, url, node):
+    def _process_item(self, url, node):
         '''Load PDF item.'''
-        href = self._get_href(url, node)
+        href = self._href(url, node)
         loader = PDFLoader(selector=node)
         loader.add_value('url', url)
         loader.add_value('href', href)
@@ -69,10 +63,12 @@ class SitesSpider(Spider):
 
     def start_requests(self):
         '''Dispatch requests per site url.'''
-        for url in self.input_list:
-            yield Request(url)
+        for row in self.data:
+            yield Request(row.get('url'), meta=self._meta(row))
 
     def parse(self, response):
         '''Parse search results.'''
         for node in response.xpath('//a[contains(@href,".pdf")]'):
-            yield self._load_item(response.url, node)
+            item = self._process_item(response.url, node)
+            item = self._process_meta(item, response.meta)
+            yield item
